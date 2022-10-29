adb76040678be286939c88996fa0c231c2ab2ddf
kapustb <boriskokap@gmail.com>
2022-10-29 17:44:07 +0200

    squares added

---
+++ factorial/Libraries/Square.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Libraries
+{
+    public class Square
+    {
+        public static void PrintSquares(int number)
+        {
+            for (int i = 1; i < number + 1; i++)
+            Console.WriteLine(i*i);         
+        }
+    }
+}
