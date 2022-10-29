e659abbd9a33d9e926d4a8c105ca0c8c6f256a24
kapustb <boriskokap@gmail.com>
2022-10-29 17:20:51 +0200

    Created app to compute the factorials

---
+++ factorial/Libraries/Factorial.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Libraries
+{
+    public class Factorial
+    {
+        public static int Compute(int number)
+        {
+            for (int i = number - 1; i > 0; i--)
+            {
+                number *= i;
+            }
+
+            return number;
+        }
+    }
+}
